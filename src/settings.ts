d1c927af4477eb71f71dc00a4f9d756ff55f04e9
gertjan00 <33345786+gertjan00@users.noreply.github.com>
2026-03-25 00:00:26 +0100

    Settings tab

--- src/settings.ts
+++ src/settings.ts
@@ -1,15 +1,19 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
+import { App, PluginSettingTab, Setting } from "obsidian";
 import MyPlugin from "./main";
 
 export interface MyPluginSettings {
-	mySetting: string;
+	appwriteEndpoint: string;
+	appwriteProjectId: string;
+	appwriteApiKey: string;
 }
 
 export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	appwriteEndpoint: "https://<REGION>.cloud.appwrite.io/v1",
+	appwriteProjectId: "",
+	appwriteApiKey: "",
+};
 
-export class SampleSettingTab extends PluginSettingTab {
+export class MyPluginSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -18,19 +22,53 @@ export class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl("h2", { text: "Appwrite Obsidian Sync settings" });
+
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Appwrite endpoint")
+			.setDesc("The url where your Appwrite project can be reached")
+			.addText((text) => {
+				text.inputEl.style.width = "250px";
+
+				text.setPlaceholder(DEFAULT_SETTINGS.appwriteEndpoint)
+					.setValue(this.plugin.settings.appwriteEndpoint)
+					.onChange(async (value) => {
+						this.plugin.settings.appwriteEndpoint = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName("Appwrite project id")
+			.setDesc("The id of your Appwrite project")
+			.addText((text) => {
+				text.inputEl.style.width = "250px";
+
+				text.setPlaceholder(DEFAULT_SETTINGS.appwriteProjectId)
+					.setValue(this.plugin.settings.appwriteProjectId)
+					.onChange(async (value) => {
+						this.plugin.settings.appwriteProjectId = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName("Appwrite API key")
+			.setDesc("Your server API key")
+			.addText((text) => {
+				text.inputEl.style.width = "250px";
+				text.inputEl.type = "password";
+
+				text.setPlaceholder(DEFAULT_SETTINGS.appwriteApiKey)
+					.setValue(this.plugin.settings.appwriteApiKey)
+					.onChange(async (value) => {
+						this.plugin.settings.appwriteApiKey = value;
+						await this.plugin.saveSettings();
+					});
+			});
 	}
 }
